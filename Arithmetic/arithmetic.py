871bd3937db74a28ea4ace40e90f476e3130ba84
Ryan Harris <ryan@synthesis.co.za>
2025-01-07 10:24:54 +0200

    Added jupyter arithmetic notebook

--- Arithmetic/arithmetic.py
+++ Arithmetic/arithmetic.py
@@ -1,16 +1,23 @@
-
+from IPython.display import display, Math
 
 def exercises():
   x = 7
   y = -2
   z = 5
-  
+
   ans = 3 * x * (4 + y)
   print(ans)
 
   ans = -y - ((x + 3) / z)
   print(ans)
+
+
+def print_equations():
+  display( '4 + 3 = 7' )
+  display( '4 + 3 = ' + str(4+3) )
+
+  display(Math('4+3=7'))
   
 
 def driver():
-  exercises()
+  print_equations()
